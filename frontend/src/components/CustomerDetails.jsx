465f5875796aa288eeeab0509f4f46614270f747
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2023-11-22 09:28:50 +0545

    added update functionality in the customer details

--- frontend/src/components/CustomerDetails.jsx
+++ frontend/src/components/CustomerDetails.jsx
@@ -1,59 +1,141 @@
-
-import React from "react";
+import React, { useState , useEffect } from "react";
 import { useCustomersContext } from "../hooks/useCustomersContext.";
 import useAuthContext from "../hooks/useAuthcontext";
-import {AiOutlineDelete} from "react-icons/ai";
+import { AiOutlineDelete } from "react-icons/ai";
+import { BiSolidEdit } from "react-icons/bi";
 
-const CustomerDetails = ({customer}) => {
-  const {dispatch}=useCustomersContext();
-  const{shop}=useAuthContext();
+const CustomerDetails = ({ customer }) => {
+  const { dispatch } = useCustomersContext();
+  const { shop } = useAuthContext();
 
+  const [show, setShow] = useState(false);
+  const [selectedCustomer, setSelectedCustomer] = useState({
+    name: "",
+    email: "",
+    contact: "",
+  });
 
-  const handleDelete= async()=>{
-    if(!shop){
+  const showEdit = async () => {
+    setShow(true);
+  };
+
+  const handleDelete = async () => {
+    if (!shop) {
       return;
     }
-    const response= await fetch("/api/customers/"+customer._id ,{
-      method:"DELETE",
-      headers:{
-        'Authorization':`Bearer ${shop.token}`,
+    const response = await fetch("/api/customers/" + customer._id, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${shop.token}`,
       },
     });
 
-    const json=await response.json();
+    const json = await response.json();
 
-    if(response.ok){
-      dispatch({type: "DELETE_CUSTOMERS" , payload : json})
+    if (response.ok) {
+      dispatch({ type: "DELETE_CUSTOMERS", payload: json });
     }
-  }
+  };
+
+  useEffect(() => {
+    setSelectedCustomer(customer);
+  }, [customer]);
+
+  const handleChange = (customerKey, newValue) => {
+    setSelectedCustomer({ ...selectedCustomer, [customerKey]: newValue });
+  };
+
+  const handleUpdate = async () => {
+    console.log(selectedCustomer);
+    if (!shop) {
+      return;
+    }
+    const response = await fetch("/api/customers/" + customer._id, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${shop.token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(selectedCustomer),
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_CUSTOMERS", payload: json });
+    }
+  };
+
   return (
     <>
-        <div
-          className="
+      <div
+        className="
        bg-neutral-400 
       flex flex-col
       justify-center
       p-4
       "
-        >
-          <div className="flex flex-col justify-center">
-            <h1 className="text-2xl font-semibold
-            ">Customer Name : </h1>
-            <p className="p-2 ml-2">{customer.name}</p>
-          </div>
-          <div className="flex flex-col justify-center">
-            <h1 className="text-2xl font-semibold
-            ">Email : </h1>
-            <p className="p-2 ml-2">{customer.email}</p>
-          </div>
-          <div className="flex flex-col justify-center">
-            <h1 className="text-2xl font-semibold
-            ">Contact : </h1>
-            <p className="p-2 ml-2">{customer.contact}</p>
-            <AiOutlineDelete className="text-2xl text-red-700" onClick={handleDelete}/>
+      >
+        <div className="flex flex-col justify-center">
+          <h1
+            className="text-2xl font-semibold
+            "
+          >
+            Customer Name :{" "}
+          </h1>
+          <p className="p-2 ml-2">{customer.name}</p>
+        </div>
+        <div className="flex flex-col justify-center">
+          <h1
+            className="text-2xl font-semibold
+            "
+          >
+            Email :{" "}
+          </h1>
+          <p className="p-2 ml-2">{customer.email}</p>
+        </div>
+        <div className="flex flex-col justify-center">
+          <h1
+            className="text-2xl font-semibold
+            "
+          >
+            Contact :{" "}
+          </h1>
+          <p className="p-2 ml-2">{customer.contact}</p>
+          <div className="flex justify-end">
+            <AiOutlineDelete
+              className="text-2xl text-red-700"
+              onClick={handleDelete}
+            />
+            <BiSolidEdit className="text-2xl ml-3" onClick={showEdit} />
           </div>
         </div>
-      
+      </div>
+      {show && (
+        <form>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            value={selectedCustomer.name}
+            onChange={(e) => handleChange("name", e.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            value={selectedCustomer.email}
+            onChange={(e) => handleChange("email", e.target.value)}
+          />
+          <label htmlFor="contact">Contact</label>
+          <input
+            type="number"
+            value={selectedCustomer.contact}
+            onChange={(e) => handleChange("contact", e.target.value)}
+          />
+          <button className="bg-green-400" onClick={handleUpdate}>
+            Update
+          </button>
+        </form>
+      )}
     </>
   );
 };
--- frontend/src/context/CustomerContext.js
+++ frontend/src/context/CustomerContext.js
@@ -2,7 +2,6 @@ import { createContext, useReducer } from "react";
 
 export const CustomersContext = createContext();
 
-
 export const customersReducer = (state, action) => {
   switch (action.type) {
     case "SET_CUSTOMERS":
@@ -19,22 +18,36 @@ export const customersReducer = (state, action) => {
         customers: state.customers.filter((w) => w._id !== action.payload._id),
       };
 
+    case "UPDATE_CUSTOMERS":
+      const updatedCustomer = action.payload;
+
+      const updatedCustomers = state.customers.map((customer) => {
+        if (customer.id === updatedCustomer.id) {
+          return updatedCustomer;
+        }
+        return customer;
+      });
+      return {
+        ...state,
+        customers: updatedCustomers,
+      };
+
     default:
       return state;
   }
 };
 
 export const CustomersContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(customersReducer,{
+  const [state, dispatch] = useReducer(customersReducer, {
     customers: null,
-  });   //customersReducer is a function  which sets the customers state which is null initally.
+  }); //customersReducer is a function  which sets the customers state which is null initally.
 
   //   the disptach function has an object as an arguement where the type is set which show what the state change we want to make and the second payload property represents any data we need to make the change.
   //   dispatch({type:"SET_customers" , payload:[{} , {}]}) //when we call the dispatch function the workuotsReducer function is invoked.
 
   return (
-
     <CustomersContext.Provider value={{ ...state, dispatch }}>
       {children}
     </CustomersContext.Provider>
-  );}
+  );
+};
